ab351e25f79b3ac8d68231d0f6d0d2911aabaf32
S P <acosmic@gmail.com>
2023-02-06 13:44:02 +0300

    yoyoyo

--- main.py
+++ main.py
@@ -16,12 +16,17 @@ async def send_message_time(bot: Bot, text_message):
     await bot.send_message(config.group_id, text_message, parse_mode='HTML')
 
 
-scheduler.add_job(send_message_time, trigger="interval", minutes=60, args=(bot,'Я уже на сервере! Присылаю приветики каждый час!'))
-scheduler.add_job(send_message_time, 'cron', day_of_week='mon-sun', hour=19, minute=25, end_date='2023-05-30', args=(bot, 'Это сообщение приходит каждый день в 19:25'))
-scheduler.add_job(send_message_time, 'cron', day_of_week='mon-sun', hour=17, minute=15, end_date='2023-05-30', args=(bot, 'Это сообщение приходит каждый день в 17:15'))
-
-# @dp.message_handler(chat_type=[types.ChatType.SUPERGROUP, types.ChatType.GROUP], content_types=["new_chat_members"], chat_id=config.ov_test_group_id)
-
+# scheduler.add_job(send_message_time, trigger="interval", minutes=60, args=(bot,'Я уже на сервере! Присылаю приветики каждый час!'))
+# scheduler.add_job(send_message_time, 'cron', day_of_week='mon-sun', hour=19, minute=25, end_date='2023-05-30', args=(bot, 'Это сообщение приходит каждый день в 19:25'))
+scheduler.add_job(send_message_time, 'cron', day_of_week='mon-sun', hour=8, minute=00, end_date='2023-05-30', args=(bot, messages.message1))
+scheduler.add_job(send_message_time, 'cron', day_of_week='mon-sun', hour=20, minute=00, end_date='2023-05-30', args=(bot, messages.message1))
+scheduler.add_job(send_message_time, 'cron', day_of_week='mon-sun', hour=14, minute=00, end_date='2023-05-30', args=(bot, messages.message2))
+scheduler.add_job(send_message_time, 'cron', day_of_week='mon-sun', hour=10, minute=00, end_date='2023-05-30', args=(bot, messages.message3))
+scheduler.add_job(send_message_time, 'cron', day_of_week='mon,wed,fri', hour=11, minute=00, end_date='2023-05-30', args=(bot, messages.message4))
+scheduler.add_job(send_message_time, 'cron', day_of_week='tue', hour=11, minute=00, end_date='2023-05-30', args=(bot, messages.message5))
+scheduler.add_job(send_message_time, 'cron', day_of_week='tue,thu,sat', hour=19, minute=00, end_date='2023-05-30', args=(bot, messages.message6))
+scheduler.add_job(send_message_time, 'cron', day_of_week='wed,sun', hour=19, minute=30, end_date='2023-05-30', args=(bot, messages.message7))
+scheduler.add_job(send_message_time, 'cron', day_of_week='mon,wed,sat', hour=15, minute=00, end_date='2023-05-30', args=(bot, messages.message8))
 
 @dp.message_handler(chat_type=[types.ChatType.SUPERGROUP, types.ChatType.GROUP], content_types=["new_chat_members"], chat_id=config.group_id)
 async def new_member(message):
@@ -46,6 +51,28 @@ async def help_handler(message: types.Message):
     print(bot.get_chat)
     logging.info(f'{user_id=} {user_full_name} {time.asctime()}')
     await message.answer("Вот список команд, которые я умею выполнять:")
+
+
+@dp.message_handler(chat_type=[types.ChatType.SUPERGROUP, types.ChatType.GROUP])
+async def help_handler(message: types.Message):
+    if message.text == '1':
+        await message.answer(messages.message1, parse_mode='HTML')
+    if message.text == '2':
+        await message.answer(messages.message2, parse_mode='HTML')
+    if message.text == '3':
+        await message.answer(messages.message3, parse_mode='HTML')
+    if message.text == '4':
+        await message.answer(messages.message4, parse_mode='HTML')
+    if message.text == '5':
+        await message.answer(messages.message5, parse_mode='HTML')
+    if message.text == '6':
+        await message.answer(messages.message6, parse_mode='HTML')
+    if message.text == '7':
+        await message.answer(messages.message7, parse_mode='HTML')
+    if message.text == '8':
+        await message.answer(messages.message8, parse_mode='HTML')
+    else:
+        pass
 
 
 if __name__ == "__main__":
